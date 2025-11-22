7af1b0c689465e77386132fe0206b52c01e5ca4f
Faelavgtt <rafavergottii@gmail.com>
2025-11-21 20:17:36 -0400

    feat:card

--- src/public/assets/js/components/cardComponent.js
+++ src/public/assets/js/components/cardComponent.js
@@ -1,8 +1,15 @@
-const CardArtwork = ({ image, name, price, desc}) => {
+const CardArtwork = ({ image, name, price, desc }) => {
     return `
-
-   
+        <div class="card-artwork">
+            <img src="${image}" alt="${name}" class="card-artwork-image"/>
+            <h3 class="card-artwork-name">${name}</h3>
+            <p class="card-artwork-desc">${desc}</p>
+            <p class="card-artwork-price">R$ ${price}</p>
+            <button class="card-artwork-button">
+                Comprar Desenho!
+            </button>
+        </div>
     `;
-}
-                    
+};
+
 export default CardArtwork;
